1b3435e0b93ec32b49b26f805d8479fd916ff4bb
Hirake <Robin.berg@hotmail.nl>
2023-12-14 22:22:05 +0100

    moved navbar and footer to running module

---
+++ src/app/running/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { Cart } from '../../models/cart.model';
+
+@Component({
+  selector: 'navbar-component',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  amountInCart: number = 0;
+  isLoggedIn: boolean = false;
+
+  constructor(private router: Router, private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.amountInCart = this.cartService.getCurrentAmountInCart();
+    this.cartService.getCartSubject().subscribe({
+      next: (cartItems: Cart[]) => {
+        this.amountInCart = cartItems.length;
+      },
+      complete: ()=>{
+      }
+    })
+  }
+
+  goToHomePage(){this.router.navigate(['home']);}
+  goToProductPage(){this.router.navigate(['product']);}
+  navigateToCart(){this.router.navigate(['checkout']);}
+}
