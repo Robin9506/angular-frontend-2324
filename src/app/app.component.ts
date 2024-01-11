b957fbc9a55188d94caadf13dddd1fe1c843c672
Hirake <Robin.berg@hotmail.nl>
2024-01-11 11:43:45 +0100

    scroll to top on router change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppModule } from './app.module';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,4 +11,15 @@ import { AppModule } from './app.module';
 })
 export class AppComponent {
   title = 'angular-webshop-2324';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
+}
 }
